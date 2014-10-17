7412472ea13b29c0031f4347a726f14da686ecb4
Mickey Vänskä <mvaenskae@gmail.com>
2014-10-17 19:46:10 +0200

    added more logic

--- 02/01_reverse-array.c
+++ 02/01_reverse-array.c
@@ -2,9 +2,12 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define XOR 1
 
 void reverse_array(size_t, uint32_t *);
-uint32_t *s_var_from_stdin (uint32_t, size_t *, size_t *);
+uint8_t *s_var_from_stdin (uint8_t, size_t *, size_t *);
+uint32_t *from_cmdline(int, char **, size_t *);
+uint32_t *from_string(size_t, uint8_t *, size_t *);
 
 int main(int argc, char **argv)
 {
@@ -23,27 +26,37 @@ int main(int argc, char **argv)
         cmdline = 1;
     }
 
+    size_t count = 0;
+
     // PROCESS DATA HERE CORRECTLY
     if (cmdline) {
-
+        uint32_t *data = from_cmdline(argc, **argv, &count);
     } else {
-
+        uint32_t *data = from_string(len, *input, &count);
     }
 
-    reverse_array(len, &data);
+    reverse_array(XOR, count, &data);
 
     return 0;
 }
 
 /*
- ** This function reverses inplace using XOR operations.
+ ** This function reverses inplace using either XOR or a temporary variable.
+ ** (xor == 1 -> use xor, else use temporary)
  */
-void reverse_array_xor(size_t len, uint32_t *array)
+void reverse_array_xor(uint8_t xor, size_t len, uint32_t *array)
 {
-    for (size_t i = 0; i < (len)>>1; ++i)
-    {
-        *(array + i) ^= *(array + len - i - 1);
-        *(array + len - i - 1) ^= *(array + i);
-        *(array + i) ^= *(array + len - i - 1);
+    if (xor) {
+        for (size_t i = 0; i < (len >> 1); ++i) {
+            *(array + i) ^= *(array + len - i - 1);
+            *(array + len - i - 1) ^= *(array + i);
+            *(array + i) ^= *(array + len - i - 1);
+        }
+    } else {
+        for (size_t = 0; i < (len >> 1); ++i) {
+            uint32_t temp = *(array + i);
+            *(array + i) = *(array + len - i - 1);
+            *(array + len - i - 1) = temp;
+        }
     }
 }
