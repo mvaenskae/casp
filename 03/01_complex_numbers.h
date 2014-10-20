593c1eddce659e0366eb50e95718b660b57ef518
Mickey Vänskä <mvaenskae@gmail.com>
2014-10-20 10:36:32 +0200

    changed answers slightly

--- 03/01_complex_numbers.h
+++ 03/01_complex_numbers.h
@@ -2,46 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct complex_number_s {
+typedef struct imaginary_number_s {
     double real;
-    double complex;
+    double imaginary;
 } cmp_num;
 
-cmp_num *add (cmp_num a, cmp_num b)
+cmp_num *add(cmp_num *, cmp_num *);
+cmp_num *subtract(cmp_num *, cmp_num *);
+cmp_num *multiply(cmp_num *, cmp_num *);
+cmp_num *divide(cmp_num *, cmp_num *);
+void print_struct(cmp_num *, cmp_num *, cmp_num *, char);
+
+cmp_num *add (cmp_num *a, cmp_num *b)
 {
     cmp_num *result = malloc(sizeof(cmp_num));
-    result->real = a.real + b.real;
-    result->complex = a.complex + b.complex;
+    result->real = a->real + b->real;
+    result->imaginary = a->imaginary + b->imaginary;
     return result;
 }
 
-cmp_num *subtract (cmp_num a, cmp_num b)
+cmp_num *subtract (cmp_num *a, cmp_num *b)
 {
-    b.real = -b.real;
-    b.complex = -b.complex;
-    return add(a, b);
-}
-
-cmp_num *multiply (cmp_num a, cmp_num b)
-{
+    cmp_num *b_neg = malloc(sizeof(cmp_num));
     cmp_num *result = malloc(sizeof(cmp_num));
-    result->real = (a.real * b.real - a.complex * b.complex);
-    result->complex = (a.real * b.complex + a.complex * b.real);
+    b_neg->real = -(b->real);
+    b_neg->imaginary = -(b->imaginary);
+    result = add(a, b_neg);
+    free(b_neg);
     return result;
 }
 
-cmp_num *divide (cmp_num a, cmp_num b)
+cmp_num *multiply (cmp_num *a, cmp_num *b)
+{
+    cmp_num *result = malloc(sizeof(cmp_num));
+    result->real = (a->real * b->real - a->imaginary * b->imaginary);
+    result->imaginary = (a->real * b->imaginary + a->imaginary * b->real);
+    return result;
+}
+
+cmp_num *divide (cmp_num *a, cmp_num *b)
 {
     cmp_num *result = malloc(sizeof(cmp_num));
     cmp_num *numerator = malloc(sizeof(cmp_num));
     cmp_num *denominator = malloc(sizeof(cmp_num));
     cmp_num *b_conj = malloc(sizeof(cmp_num));
-    b_conj->real = b.real;
-    b_conj->complex = -b.complex;
-    numerator = multiply(a, *b_conj);
-    denominator = multiply(b, *b_conj);
+    b_conj->real = b->real;
+    b_conj->imaginary = -(b->imaginary);
+    numerator = multiply(a, b_conj);
+    denominator = multiply(b, b_conj);
+    free(b_conj);
     result->real = numerator->real / denominator->real;
-    result->complex = numerator->complex / denominator->real;
+    result->imaginary = numerator->imaginary / denominator->real;
     free(numerator);
     free(denominator);
     return result;
@@ -49,5 +60,5 @@ cmp_num *divide (cmp_num a, cmp_num b)
 
 void print_struct (cmp_num *a, cmp_num *b, cmp_num *res, char c)
 {
-    printf("(%f + %fi) %c (%f + %fi) = (%f + %fi)\n", a->real, a->complex, c, b->real, b->complex, res->real, res->complex);
+    printf("(%f + %fi) %c (%f + %fi) = (%f + %fi)\n", a->real, a->imaginary, c, b->real, b->imaginary, res->real, res->imaginary);
 }
