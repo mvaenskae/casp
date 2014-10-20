9d1ec24ab99f67ae67d80eb761f90288058b2cf5
Mickey Vänskä <mvaenskae@gmail.com>
2014-10-20 11:30:01 +0200

    accomodates answers according to question 2

--- 03/01_complex_numbers.c
+++ 03/01_complex_numbers.c
@@ -5,25 +5,19 @@
 
 int main(int argc, char **argv)
 {
-    cmp_num *a = malloc(sizeof(cmp_num));
-    cmp_num *b = malloc(sizeof(cmp_num));
-    a->real = 5.5;
-    a->imaginary = 1.25;
-    b->real = 2.75;
-    b->imaginary = 3.25;
+    cmp_num a = {5.5, 1.25};
+    cmp_num b = {2.75, 3.25};
     cmp_num *ad = add(a, b);
     cmp_num *sub = subtract(a, b);
     cmp_num *mult = multiply(a, b);
     cmp_num *div = divide(a, b);
-    print_struct(a, b, ad, '+');
+    print_struct(a, b, *ad, '+');
     free(ad);
-    print_struct(a, b, sub, '-');
+    print_struct(a, b, *sub, '-');
     free(sub);
-    print_struct(a, b, mult, '*');
+    print_struct(a, b, *mult, '*');
     free(mult);
-    print_struct(a, b, div, '/');
-    free(a);
-    free(b);
+    print_struct(a, b, *div, '/');
     free(div);
     return 0;
 }
--- 03/01_complex_numbers.h
+++ 03/01_complex_numbers.h
@@ -1,55 +1,61 @@
-#include <inttypes.h>
+#include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct imaginary_number_s {
+typedef struct complex_number_s {
     double real;
     double imaginary;
 } cmp_num;
 
-cmp_num *add(cmp_num *, cmp_num *);
-cmp_num *subtract(cmp_num *, cmp_num *);
-cmp_num *multiply(cmp_num *, cmp_num *);
-cmp_num *divide(cmp_num *, cmp_num *);
-void print_struct(cmp_num *, cmp_num *, cmp_num *, char);
+typedef struct complex_set_s {
+    int num_points_in_set;
+    cmp_num *points;
+} cmp_set;
 
-cmp_num *add (cmp_num *a, cmp_num *b)
+cmp_num *add(cmp_num, cmp_num);
+cmp_num *subtract(cmp_num, cmp_num);
+cmp_num *multiply(cmp_num, cmp_num);
+cmp_num *divide(cmp_num, cmp_num);
+cmp_set *alloc_set(cmp_num *, int);
+void free_set(cmp_set *);
+void print_struct(cmp_num, cmp_num, cmp_num, char);
+
+cmp_num *add (cmp_num a, cmp_num b)
 {
     cmp_num *result = malloc(sizeof(cmp_num));
-    result->real = a->real + b->real;
-    result->imaginary = a->imaginary + b->imaginary;
+    result->real = a.real + b.real;
+    result->imaginary = a.imaginary + b.imaginary;
     return result;
 }
 
-cmp_num *subtract (cmp_num *a, cmp_num *b)
+cmp_num *subtract (cmp_num a, cmp_num b)
 {
     cmp_num *b_neg = malloc(sizeof(cmp_num));
     cmp_num *result = malloc(sizeof(cmp_num));
-    b_neg->real = -(b->real);
-    b_neg->imaginary = -(b->imaginary);
-    result = add(a, b_neg);
+    b_neg->real = -(b.real);
+    b_neg->imaginary = -(b.imaginary);
+    result = add(a, *b_neg);
     free(b_neg);
     return result;
 }
 
-cmp_num *multiply (cmp_num *a, cmp_num *b)
+cmp_num *multiply (cmp_num a, cmp_num b)
 {
     cmp_num *result = malloc(sizeof(cmp_num));
-    result->real = (a->real * b->real - a->imaginary * b->imaginary);
-    result->imaginary = (a->real * b->imaginary + a->imaginary * b->real);
+    result->real = (a.real * b.real - a.imaginary * b.imaginary);
+    result->imaginary = (a.real * b.imaginary + a.imaginary * b.real);
     return result;
 }
 
-cmp_num *divide (cmp_num *a, cmp_num *b)
+cmp_num *divide (cmp_num a, cmp_num b)
 {
     cmp_num *result = malloc(sizeof(cmp_num));
     cmp_num *numerator = malloc(sizeof(cmp_num));
     cmp_num *denominator = malloc(sizeof(cmp_num));
     cmp_num *b_conj = malloc(sizeof(cmp_num));
-    b_conj->real = b->real;
-    b_conj->imaginary = -(b->imaginary);
-    numerator = multiply(a, b_conj);
-    denominator = multiply(b, b_conj);
+    b_conj->real = b.real;
+    b_conj->imaginary = -(b.imaginary);
+    numerator = multiply(a, *b_conj);
+    denominator = multiply(b, *b_conj);
     free(b_conj);
     result->real = numerator->real / denominator->real;
     result->imaginary = numerator->imaginary / denominator->real;
@@ -58,7 +64,26 @@ cmp_num *divide (cmp_num *a, cmp_num *b)
     return result;
 }
 
-void print_struct (cmp_num *a, cmp_num *b, cmp_num *res, char c)
+cmp_set *alloc_set(cmp_num *c_arr, int size)
 {
-    printf("(%f + %fi) %c (%f + %fi) = (%f + %fi)\n", a->real, a->imaginary, c, b->real, b->imaginary, res->real, res->imaginary);
+    cmp_set *set = malloc(sizeof(cmp_set));
+    set->num_points_in_set = size;
+    assert(size > 0);
+    set->points = malloc(sizeof(cmp_num) * (unsigned) size);
+    //fill array?
+    set->points = c_arr;
+    return set;
+}
+
+void free_set(cmp_set *set)
+{
+    for (int i = 0; i < set->num_points_in_set; ++i) {
+        free(set->points);
+    }
+    free(set);
+}
+
+void print_struct (cmp_num a, cmp_num b, cmp_num res, char c)
+{
+    printf("(%f + %fi) %c (%f + %fi) = (%f + %fi)\n", a.real, a.imaginary, c, b.real, b.imaginary, res.real, res.imaginary);
 }
