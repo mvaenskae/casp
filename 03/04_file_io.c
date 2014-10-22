497391d60fe6963b56dd5cd24939de7f763d40e9
Mickey Vänskä <mvaenskae@gmail.com>
2014-10-22 19:10:31 +0200

    still buggy as hell

--- 03/04_file_io.c
+++ 03/04_file_io.c
@@ -20,7 +20,8 @@ int main(int argc, char **argv)
     const char *origin = "\"25\"";
     size_t total = 0;
     stapo_list *list = malloc(sizeof(stapo_list));
-    list->element = malloc(sizeof(stapo));
+    stapo *states = malloc(sizeof(stapo));
+    //list->element = malloc(sizeof(stapo));
     list->count = 0; // For init purposes...
 
     // Try opening the file for reading
@@ -36,57 +37,81 @@ int main(int argc, char **argv)
     // More fun trying to decipher when drunk!
     for(char c = EOF; (c = getc(fp)) != EOF; ) {
         if (c == '\n') {
-            char *t = malloc(sizeof(char) * 5);
+            char *t = calloc(5, sizeof(char));
             if (fgets(t, 5, fp) != NULL) {
-                if (strcmp(t, origin)) {
+                if (!strcmp(t, origin)) {
                     uint8_t i = 0;
-                    stapo *elem = malloc(sizeof(stapo));
-                    (list->count)++;
-                    list->element[list->count - 1] = *elem;
+                    stapo *elem = calloc(1, sizeof(stapo));
                     for (; (c = getc(fp)) != EOF && c != '\n'; ) {
                         if (c == '\t') {
                             ++i;
-                        }
-                        if (i == state) {
-                            uint8_t len = 0;
-                            /*
-                             ** This is a lazy init, Do NOT blindly copy this.
-                             ** You have been warned!
-                             */
-                            char *s = malloc(sizeof(char) * 25);
-                            for(; (c = getc(fp)) != '\t'; ++len) {
-                                s[len] = c;
+                            if (i == state) {
+                                uint8_t len = 0;
+                                /*
+                                 ** This is a lazy init, Do NOT blindly copy
+                                 ** this. You have been warned!
+                                 */
+                                char *s = calloc(25, sizeof(char));
+                                for(; (c = getc(fp)) != ' '; ++len) {
+                                    s[len] = c;
+                                }
+                                ++len;
+                                s[len] = '\0';
+                                elem->state = malloc(sizeof(char) * len);
+                                for(int k = 0; k < len; ++k) {
+                                    elem->state[k] = s[k];
+                                }
+                                free(s);
+                                s = NULL;
+                                continue;
                             }
-                            ++len;
-                            s[len] = '\0';
-                            elem->state = malloc(sizeof(char) * len);
-                            for(int k = 0; k < len; ++k) {
-                                elem->state[k] = s[k];
-                            }
-                            free(s);
-                            ++i;
-                        }
-                        if (i == population) {
-                            uint8_t len = 0;
-                            /*
-                             ** This is also a lazy init! Do NOT blindly copy
-                             ** this either! You have been warned, again!
-                             */
-                            char *s = malloc(sizeof(char) * 11);
-                            uint64_t temp = 0;
-                            for(; (c = getc(fp)) != '\n'; ++len) {
-                                s[len] = c;
-                            }
-                            temp = strtoull(s, NULL, 0);
-                            if (temp < SIZE_MAX) {
-                                elem->population = (size_t) temp;
+                            if (i == population) {
+                                uint8_t len = 0;
+                                /*
+                                 ** This is also a lazy init! Do NOT blindly
+                                 ** copy this either! You have been warned,
+                                 ** again!
+                                 */
+                                char *s = malloc(sizeof(char) * 11);
+                                uint64_t temp = 0;
+                                for(; (c = getc(fp)) != '\n'; ++len) {
+                                    s[len] = c;
+                                }
+                                ++len;
+                                s[len] = '\0';
+                                temp = strtoull(s, NULL, 0);
+                                free(s);
+                                s = NULL;
+                                if (temp < SIZE_MAX) {
+                                    elem->population = (size_t) temp;
+                                }
+                                /*
+                                 ** Non-correct error handling here, will clean
+                                 ** it up later.
+                                 */
+                                break;
                             }
                         }
                     }
+                    ++(list->count);
+                    stapo *temp = realloc(states, sizeof(stapo) * list->count);
+                    if (temp == NULL) {
+                        free(list);
+                        free(states);
+                        free(elem);
+                        printf("OOM!\n");
+                        return -1;
+                    }
+                    states = temp;
+                    states[list->count - 1] = *elem;
                 }
+            } else {
+                free(t);
+                t = NULL;
             }
         }
     }
+    list->element = states;
     fclose(fp);
 
     /* This print is not completely safe but works for the assignment
@@ -97,12 +122,13 @@ int main(int argc, char **argv)
     for(size_t i = 0; i < list->count; ++i) {
         printf("%20s %5c %9zd\n", (list->element[i]).state, ' ', (list->element[i]).population);
         total += (list->element[i]).population;
-        // free elements
         free((list->element[i]).state);
         free(&(list->element[i]));
+        list->element[i].state = NULL;
+        //(list->element[i]) = NULL;
     }
-    //free(list->element);
     free(list);
+    list = NULL;
     printf("%34c\n%20s %5c %9zd\n", '-', "TOTAL", ' ', total);
 
    return 0;
